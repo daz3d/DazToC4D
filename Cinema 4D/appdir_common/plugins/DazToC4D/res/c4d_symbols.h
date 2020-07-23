d2ff1c9f3b413490b3464b510ed7c7de584ee771
rbtwhiz <20308717+rbtwhiz@users.noreply.github.com>
2020-07-23 17:55:57 -0600

    Initial code commit - Marcello's submission with changes.

---
+++ Cinema 4D/appdir_common/plugins/DazToC4D/res/c4d_symbols.h
@@ -0,0 +1,18 @@
+// C4D-Plugin Symbols
+
+enum
+{
+	PLUGIN_ID    = 10336071,
+	IDS_AUTH                                        = 40000,
+	IDS_AUTH_DIALOG                                 = 40001,
+	DLG_AUTH                                        = 41000,
+	IDC_SERIAL                                      = 41001,
+	IDC_SUBMIT                                      = 41002,
+	IDC_CANCEL                                      = 41003,
+	IDC_CODEBOX                                     = 41004,
+	IDC_CODEBOX_AUTH                                = 41005,
+	IDC_PASTESERIAL                                 = 41007,
+    
+// End of symbol definition
+  _DUMMY_ELEMENT_
+};
