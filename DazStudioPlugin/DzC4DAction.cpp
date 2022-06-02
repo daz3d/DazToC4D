9dbed3ceb127c930f5fdd3dea6724b692d581b8f
danielbui78 <danielbui78@gmail.com>
2022-06-02 13:22:34 -0400

    Initial Commit: C++ refactor, Daz Bridge Library integration

---
+++ DazStudioPlugin/DzC4DAction.cpp
@@ -0,0 +1,311 @@
+#include <QtGui/qcheckbox.h>
+#include <QtGui/QMessageBox>
+#include <QtNetwork/qudpsocket.h>
+#include <QtNetwork/qabstractsocket.h>
+#include <QCryptographicHash>
+#include <QtCore/qdir.h>
+
+#include <dzapp.h>
+#include <dzscene.h>
+#include <dzmainwindow.h>
+#include <dzshape.h>
+#include <dzproperty.h>
+#include <dzobject.h>
+#include <dzpresentation.h>
+#include <dznumericproperty.h>
+#include <dzimageproperty.h>
+#include <dzcolorproperty.h>
+#include <dpcimages.h>
+
+#include "QtCore/qmetaobject.h"
+#include "dzmodifier.h"
+#include "dzgeometry.h"
+#include "dzweightmap.h"
+#include "dzfacetshape.h"
+#include "dzfacetmesh.h"
+#include "dzfacegroup.h"
+#include "dzprogress.h"
+
+#include "DzC4DAction.h"
+#include "DzC4DDialog.h"
+#include "DzBridgeMorphSelectionDialog.h"
+#include "DzBridgeSubdivisionDialog.h"
+
+#ifdef WIN32
+#include <shellapi.h>
+#endif
+
+#include "dzbridge.h"
+
+DzC4DAction::DzC4DAction() :
+	DzBridgeAction(tr("Daz To &Cinema 4D"), tr("Send the selected node to Cinema 4D."))
+{
+	m_nNonInteractiveMode = 0;
+	m_sAssetType = QString("SkeletalMesh");
+	//Setup Icon
+	QString iconName = "Daz to Cinema 4D";
+	QPixmap basePixmap = QPixmap::fromImage(getEmbeddedImage(iconName.toLatin1()));
+	QIcon icon;
+	icon.addPixmap(basePixmap, QIcon::Normal, QIcon::Off);
+	QAction::setIcon(icon);
+
+}
+
+bool DzC4DAction::createUI()
+{
+	// Check if the main window has been created yet.
+	// If it hasn't, alert the user and exit early.
+	DzMainWindow* mw = dzApp->getInterface();
+	if (!mw)
+	{
+		if (m_nNonInteractiveMode == 0) QMessageBox::warning(0, tr("Error"),
+			tr("The main window has not been created yet."), QMessageBox::Ok);
+
+		return false;
+	}
+
+	// m_subdivisionDialog creation REQUIRES valid Character or Prop selected
+	if (dzScene->getNumSelectedNodes() != 1)
+	{
+		if (m_nNonInteractiveMode == 0) QMessageBox::warning(0, tr("Error"),
+			tr("Please select one Character or Prop to send."), QMessageBox::Ok);
+
+		return false;
+	}
+
+	 // Create the dialog
+	if (!m_bridgeDialog)
+	{
+		m_bridgeDialog = new DzC4DDialog(mw);
+	}
+	else
+	{
+		DzC4DDialog* c4dDialog = qobject_cast<DzC4DDialog*>(m_bridgeDialog);
+		if (c4dDialog)
+		{
+			c4dDialog->resetToDefaults();
+			c4dDialog->loadSavedSettings();
+		}
+	}
+
+	if (!m_subdivisionDialog) m_subdivisionDialog = DZ_BRIDGE_NAMESPACE::DzBridgeSubdivisionDialog::Get(m_bridgeDialog);
+	if (!m_morphSelectionDialog) m_morphSelectionDialog = DZ_BRIDGE_NAMESPACE::DzBridgeMorphSelectionDialog::Get(m_bridgeDialog);
+
+	return true;
+}
+
+void DzC4DAction::executeAction()
+{
+	// CreateUI() disabled for debugging -- 2022-Feb-25
+	/*
+		 // Create and show the dialog. If the user cancels, exit early,
+		 // otherwise continue on and do the thing that required modal
+		 // input from the user.
+		 if (createUI() == false)
+			 return;
+	*/
+
+	// Check if the main window has been created yet.
+	// If it hasn't, alert the user and exit early.
+	DzMainWindow* mw = dzApp->getInterface();
+	if (!mw)
+	{
+		if (m_nNonInteractiveMode == 0)
+		{
+			QMessageBox::warning(0, tr("Error"),
+				tr("The main window has not been created yet."), QMessageBox::Ok);
+		}
+		return;
+	}
+
+	// Create and show the dialog. If the user cancels, exit early,
+	// otherwise continue on and do the thing that required modal
+	// input from the user.
+	if (dzScene->getNumSelectedNodes() != 1)
+	{
+		if (m_nNonInteractiveMode == 0)
+		{
+			QMessageBox::warning(0, tr("Error"),
+				tr("Please select one Character or Prop to send."), QMessageBox::Ok);
+		}
+		return;
+	}
+
+	// Create the dialog
+	if (m_bridgeDialog == nullptr)
+	{
+		m_bridgeDialog = new DzC4DDialog(mw);
+	}
+	else
+	{
+		if (m_nNonInteractiveMode == 0)
+		{
+			m_bridgeDialog->resetToDefaults();
+			m_bridgeDialog->loadSavedSettings();
+		}
+	}
+
+	// Prepare member variables when not using GUI
+	if (m_nNonInteractiveMode == 1)
+	{
+//		if (m_sRootFolder != "") m_bridgeDialog->getIntermediateFolderEdit()->setText(m_sRootFolder);
+
+		if (m_aMorphListOverride.isEmpty() == false)
+		{
+			m_bEnableMorphs = true;
+			m_sMorphSelectionRule = m_aMorphListOverride.join("\n1\n");
+			m_sMorphSelectionRule += "\n1\n.CTRLVS\n2\nAnything\n0";
+			if (m_morphSelectionDialog == nullptr)
+			{
+				m_morphSelectionDialog = DZ_BRIDGE_NAMESPACE::DzBridgeMorphSelectionDialog::Get(m_bridgeDialog);
+			}
+			m_mMorphNameToLabel.clear();
+			foreach(QString morphName, m_aMorphListOverride)
+			{
+				QString label = m_morphSelectionDialog->GetMorphLabelFromName(morphName);
+				m_mMorphNameToLabel.insert(morphName, label);
+			}
+		}
+		else
+		{
+			m_bEnableMorphs = false;
+			m_sMorphSelectionRule = "";
+			m_mMorphNameToLabel.clear();
+		}
+
+	}
+
+	// If the Accept button was pressed, start the export
+	int dlgResult = -1;
+	if (m_nNonInteractiveMode == 0)
+	{
+		dlgResult = m_bridgeDialog->exec();
+	}
+	if (m_nNonInteractiveMode == 1 || dlgResult == QDialog::Accepted)
+	{
+		// DB 2021-10-11: Progress Bar
+		DzProgress* exportProgress = new DzProgress("Sending to Cinema 4D...", 10);
+
+		// Read Common GUI values
+		readGui(m_bridgeDialog);
+
+		// Read Custom GUI values
+//		DzC4DDialog* c4dDialog = qobject_cast<DzC4DDialog*>(m_bridgeDialog);
+
+#if __LEGACY_PATHS__
+		m_sExportFbx = "B_FIG";
+		m_sAssetName = "FIG";
+		m_sDestinationPath = m_sRootFolder + "/";
+		m_sDestinationFBX = m_sDestinationPath + m_sExportFbx + ".fbx";
+#endif
+
+		//Create Daz3D folder if it doesn't exist
+		QDir dir;
+		dir.mkpath(m_sRootFolder);
+		exportProgress->step();
+
+		exportHD(exportProgress);
+
+		// DB 2021-10-11: Progress Bar
+		exportProgress->finish();
+
+		// DB 2021-09-02: messagebox "Export Complete"
+		if (m_nNonInteractiveMode == 0)
+		{
+			QMessageBox::information(0, "Daz To Cinema 4D Bridge",
+				tr("Export phase from Daz Studio complete. Please switch to Cinema 4D to begin Import phase."), QMessageBox::Ok);
+		}
+
+	}
+}
+
+
+void DzC4DAction::writeConfiguration()
+{
+	QString DTUfilename = m_sDestinationPath + m_sAssetName + ".dtu";
+	QFile DTUfile(DTUfilename);
+	DTUfile.open(QIODevice::WriteOnly);
+	DzJsonWriter writer(&DTUfile);
+	writer.startObject(true);
+
+	writeDTUHeader(writer);
+
+	if (m_sAssetType.toLower().contains("mesh"))
+	{
+		QTextStream *pCVSStream = nullptr;
+		if (m_bExportMaterialPropertiesCSV)
+		{
+			QString filename = m_sDestinationPath + m_sAssetName + "_Maps.csv";
+			QFile file(filename);
+			file.open(QIODevice::WriteOnly);
+			pCVSStream = new QTextStream(&file);
+			*pCVSStream << "Version, Object, Material, Type, Color, Opacity, File" << endl;
+		}
+		writeAllMaterials(m_pSelectedNode, writer, pCVSStream);
+		writeAllMorphs(writer);
+
+		writer.startMemberObject("MorphLinks");
+		writer.finishObject();
+		writer.startMemberArray("MorphNames");
+		writer.finishArray();
+
+		DzBoneList aBoneList = getAllBones(m_pSelectedNode);
+
+		writeSkeletonData(m_pSelectedNode, writer);
+		writeHeadTailData(m_pSelectedNode, writer);
+
+		writeJointOrientation(aBoneList, writer);
+		writeLimitData(aBoneList, writer);
+		writePoseData(m_pSelectedNode, writer, true);
+		writeAllSubdivisions(writer);
+		writeAllDforceInfo(m_pSelectedNode, writer);
+	}
+
+	if (m_sAssetType == "Pose")
+	{
+	   writeAllPoses(writer);
+	}
+
+	if (m_sAssetType == "Environment")
+	{
+		writeEnvironment(writer);
+	}
+
+	writer.finishObject();
+	DTUfile.close();
+
+}
+
+// Setup custom FBX export options
+void DzC4DAction::setExportOptions(DzFileIOSettings& ExportOptions)
+{
+	ExportOptions.setBoolValue("doEmbed", false);
+	ExportOptions.setBoolValue("doDiffuseOpacity", false);
+	ExportOptions.setBoolValue("doCopyTextures", false);
+
+}
+
+QString DzC4DAction::readGuiRootFolder()
+{
+	QString rootFolder = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation) + QDir::separator() + "DazToC4D";
+#if __LEGACY_PATHS__
+		rootFolder = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation) + "/DAZ 3D/Bridges/Daz To Cinema 4D/Exports/FIG/FIG0";
+		rootFolder = rootFolder.replace("\\","/");
+#else
+	if (m_bridgeDialog)
+	{
+		QLineEdit* intermediateFolderEdit = nullptr;
+		DzC4DDialog* c4dDialog = qobject_cast<DzC4DDialog*>(m_bridgeDialog);
+
+		if (c4dDialog)
+			intermediateFolderEdit = c4dDialog->getAssetsFolderEdit();
+
+		if (intermediateFolderEdit)
+			rootFolder = intermediateFolderEdit->text().replace("\\", "/") + "/Daz3D";
+	}
+#endif
+
+	return rootFolder;
+}
+
+#include "moc_DzC4DAction.cpp"
