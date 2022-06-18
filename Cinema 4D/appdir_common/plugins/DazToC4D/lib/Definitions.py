e001d59bf60bb07d236833a67a7cec2a5092e43f
danielbui78 <danielbui78@gmail.com>
2022-06-18 14:43:07 -0400

    bugfix custom Documents path
    - also rename DLL\DYLIB filename

--- Cinema 4D/appdir_common/plugins/DazToC4D/lib/Definitions.py
+++ Cinema 4D/appdir_common/plugins/DazToC4D/lib/Definitions.py
@@ -1,7 +1,19 @@
 import os
+import platform
 
-HOME_DIR = os.path.expanduser("~")
-ROOT_DIR = os.path.join(HOME_DIR, "Documents", "DAZ 3D", "Bridges", "Daz To Cinema 4D")
+if (platform.system() == "Windows"):
+    import ctypes.wintypes
+    CSIDL_PERSONAL=5
+    SHGFP_TYPE_CURRENT=0
+    buffer = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(0, CSIDL_PERSONAL, 0, SHGFP_TYPE_CURRENT, buffer)
+    HOME_DIR = buffer.value
+elif (platform.system() == "Darwin"):
+    HOME_DIR = os.path.expanduser("~") + "/Documents"
+else:
+    HOME_DIR = os.path.expanduser("~")
+
+ROOT_DIR = os.path.join(HOME_DIR, "DAZ 3D", "Bridges", "Daz To Cinema 4D")
 EXPORT_DIR = os.path.join(ROOT_DIR, "Exports")
 
 LIB_DIR = os.path.dirname(__file__)
